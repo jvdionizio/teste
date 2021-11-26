bbf227c154a5716937e2a03dd333d602f0f215ca
jvdionizio <dionizio145@gmail.com>
2021-11-26 16:07:40 -0300

    adicionando exercicios de fixação

---
+++ fundamentos/bloco-04-Introdução-à-JS-e-LP/switch-case.js
@@ -0,0 +1,16 @@
+// switch -- case
+let trafficLight = "roxo";
+
+switch (trafficLight) {
+  case "vermelho":
+    console.log("pare");
+    break;
+  case "amarelo":
+    console.log("atenção");
+    break;
+  case "verde":
+    console.log("siga");
+    break;
+  default:
+    console.log("variavel não identificada")
+}
