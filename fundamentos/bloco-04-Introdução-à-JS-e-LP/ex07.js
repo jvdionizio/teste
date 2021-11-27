f0e42f4b5f4df8d840dd237b3e4b6dac405f0614
jvdionizio <dionizio145@gmail.com>
2021-11-27 12:47:40 -0300

    Adicionando exs até o 8

---
+++ fundamentos/bloco-04-Introdução-à-JS-e-LP/ex07.js
@@ -0,0 +1,26 @@
+let nota = 45;
+
+if (nota > 100){
+  console.log("Essa não é uma nota válida");
+}
+else if (nota < 0) {
+  console.log("Essa não é uma nota válida");
+}
+else if (nota >= 90){
+  console.log(" nota A")
+}
+else if (nota >= 80){
+  console.log(" nota B")
+}
+else if (nota >= 70){
+  console.log(" nota C")
+}
+else if (nota >= 60){
+  console.log(" nota D")
+}
+else if (nota >= 50){
+  console.log(" nota E")
+}
+else if (nota < 50){
+  console.log(" nota F")
+}
