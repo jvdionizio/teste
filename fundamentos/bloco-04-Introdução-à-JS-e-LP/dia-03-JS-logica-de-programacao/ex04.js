4df6bdd2b1ee984afea869e3b0c4f842bd9e51bc
jvdionizio <dionizio145@gmail.com>
2021-11-30 21:08:24 -0300

    ex04 dia 3, bloco 4

---
+++ fundamentos/bloco-04-Introdução-à-JS-e-LP/dia-03-JS-logica-de-programacao/ex04.js
@@ -0,0 +1,19 @@
+let n = 5;
+let espaco = " ";
+let asterisco = "*"
+
+if (n>1 && n%2 == 1) {
+  for (let i = 1; i <= n; i += 2){
+    let linhaAsteriscos = "";
+    for (let j = (n-i); j >= 0; j -= 2){
+      linhaAsteriscos = linhaAsteriscos + espaco;
+    }
+    for (let k = 1; k <= i; k++){
+      linhaAsteriscos = linhaAsteriscos + asterisco;
+    }
+    console.log(linhaAsteriscos);
+  }
+}
+else {
+  console.log("Seu n é menor que 1 ou é par");
+}
