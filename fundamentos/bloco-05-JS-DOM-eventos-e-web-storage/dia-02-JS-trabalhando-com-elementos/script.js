2bf051ba63395a07861eb5df0c43d7c08f2a03d6
jvdionizio <dionizio145@gmail.com>
2021-12-07 19:31:33 -0300

    terminando parte 1

--- fundamentos/bloco-05-JS-DOM-eventos-e-web-storage/dia-02-JS-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-JS-DOM-eventos-e-web-storage/dia-02-JS-trabalhando-com-elementos/script.js
@@ -1,1 +1,14 @@
-console.log(document.getElementById("lista").innerText);
+const pai = document.querySelector("#elementoOndeVoceEsta").parentElement;
+pai.style.color = 'red'
+
+document.querySelector("#primeiroFilhodoFilho").innerText = 'Primeiro filho pai, slc';
+
+console.log(document.querySelector('#pai').firstElementChild);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').parentElement.firstElementChild);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling.nextElementSibling)
+
+console.log(document.querySelector('#pai').lastElementChild.previousElementSibling);
