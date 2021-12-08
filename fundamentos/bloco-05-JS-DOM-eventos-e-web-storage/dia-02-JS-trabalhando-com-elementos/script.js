36338ed7b29a76012e9cf10c488e35fe80de4004
jvdionizio <dionizio145@gmail.com>
2021-12-08 15:12:34 -0300

    Terminado o dia 2

--- fundamentos/bloco-05-JS-DOM-eventos-e-web-storage/dia-02-JS-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-JS-DOM-eventos-e-web-storage/dia-02-JS-trabalhando-com-elementos/script.js
@@ -12,3 +12,25 @@ console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
 console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling.nextElementSibling)
 
 console.log(document.querySelector('#pai').lastElementChild.previousElementSibling);
+
+let irmão = document.createElement('div');
+irmão.innerText = 'farofadagkay';
+pai.appendChild(irmão);
+
+let filhao= document.createElement('div');
+filhao.innerText = 'é o filhão, né pai?';
+filhao.style.margin = '30px';
+filhao.className = 'filhas'
+document.querySelector('#elementoOndeVoceEsta').appendChild(filhao);
+
+let filho = document.createElement('div');
+filho.innerText = 'é o filho do irmão do filhão, ai tomaaa!!!!';
+filho.style.margin = '60px';
+document.querySelector('#primeiroFilhodoFilho').appendChild(filho);
+
+console.log(filho.parentElement.parentElement.nextElementSibling)
+
+let filhao2 = document.querySelector('.filhas')
+if (filhao2.innerText.includes('né')) {
+  document.querySelector('#elementoOndeVoceEsta').removeChild(filhao2);
+}
