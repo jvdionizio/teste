2ee6263227e0ffd5077477d69093bee87681f418
jvdionizio <dionizio145@gmail.com>
2022-02-17 21:30:32 -0300

    Add helloworld and pokedex

---
+++ frontend/bloco-10-introducao-a-react/dia-01-Hello-World-No-React/hello-world/src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+  render() {
+    return <span className="HelloWorld">Hello World</span>
+  }
+}
+export default HelloWorld
